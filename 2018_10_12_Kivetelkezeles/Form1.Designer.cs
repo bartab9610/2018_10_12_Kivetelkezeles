85d301193230b26864a3f0cfea2d1a317e2c5a91
Petrik Lajos Szg <info@petrik.hu>
2018-10-12 14:51:01 +0200

    Létrehozás

---
+++ 2018_10_12_Kivetelkezeles/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _2018_10_12_Kivetelkezeles
+{
+    partial class Form_kivetelkezeles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox_path = new System.Windows.Forms.TextBox();
+            this.Button_create_file = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox_path
+            // 
+            this.TextBox_path.Location = new System.Drawing.Point(22, 20);
+            this.TextBox_path.Name = "TextBox_path";
+            this.TextBox_path.Size = new System.Drawing.Size(100, 20);
+            this.TextBox_path.TabIndex = 0;
+            // 
+            // Button_create_file
+            // 
+            this.Button_create_file.Location = new System.Drawing.Point(164, 20);
+            this.Button_create_file.Name = "Button_create_file";
+            this.Button_create_file.Size = new System.Drawing.Size(92, 23);
+            this.Button_create_file.TabIndex = 1;
+            this.Button_create_file.Text = "Fájl létrehozás";
+            this.Button_create_file.UseVisualStyleBackColor = true;
+            // 
+            // Form_kivetelkezeles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 71);
+            this.Controls.Add(this.Button_create_file);
+            this.Controls.Add(this.TextBox_path);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form_kivetelkezeles";
+            this.Text = "Kivételkezelés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_path;
+        private System.Windows.Forms.Button Button_create_file;
+    }
+}
+
---
+++ 2018_10_12_Kivetelkezeles/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2018_10_12_Kivetelkezeles
+{
+    public partial class Form_kivetelkezeles : Form
+    {
+        public Form_kivetelkezeles()
+        {
+            InitializeComponent();
+
+            Button_create_file.Click += (sender, e) =>
+            {
+                try
+                {
+                    string Fajl_nev = TextBox_path.Text;
+                    File.WriteAllText(Fajl_nev, "Hello");
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Érvénytelen fájlnév, adjon meg egy úját!");
+                }
+            };
+        }
+    }
+}
